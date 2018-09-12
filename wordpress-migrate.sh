7bf5094ac6e287a57015885a3c072e8297de89ca
Aaron Seibert <aaron@binaryitsystems.com>
2018-09-12 14:34:22 -0400

    oops.

--- wordpress-migrate.sh
+++ wordpress-migrate.sh
@@ -5,6 +5,8 @@ bucket=$AWS_WP_BUCKET
 content_file=$domain.tgz
 db_file=$domain.sql
 
+"${AWS_WP_BUCKET:?AWS_WP_BUCKET must be set!}"
+
 # Get files
 echo "\nRetrieving $content_file from $bucket:"
 aws s3 sync s3://$bucket/$content_file . --exclude "*" --include "$content_file"
@@ -18,3 +20,10 @@ kubectl cp $content_file $pod:/wp-content.tgz
 
 echo "\nUploading $db_file to $pod:"
 kubectl cp $db_file $pod:/db.sql
+
+# Executing pod script
+echo "\nExecuting POD script..."
+kubectl exec $pod -- /usr/bin/curl https://raw.githubusercontent.com/AaronSeibert/k8s-wordpress-migration/master/pod_script.sh | /bin/bash
+
+# Clean Up
+echo "\nCleaning up local files..."
